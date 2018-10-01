724167e82270ede615c0e4f3f534f664c118f188
tomassandstrom <tes1005@wildcats.unh.edu>
2018-09-30 22:01:34 -0400

    Detecting Controller Input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <unistd.h>
+
+using namespace std;
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+
+    cout << "Starting GLFW" << endl;
+    if (!glfwInit())
+    {
+        cout << "GLFW Failed To Start" << endl;
+    }
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while(true) {
+        int count;
+        const float* axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &count);
+
+        cout<<"Count of axes:"<<count-1<<" axes are:";
+        for(int i =0; i < count; i++){
+            cout<<axes[i]<< " ";
+        }
+        cout<<endl;
+
+        usleep(500000);
+    }
+#pragma clang diagnostic pop
+
+
 }
+
